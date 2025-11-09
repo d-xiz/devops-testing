f443bdb53d3d7df1bd39a5870c17118456f26dfd
danny <40354680+Troaxx@users.noreply.github.com>
2025-11-09 14:55:47 +0800

    Fixed imports

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs').promises;
 
 // Import utility modules for each CRUD operation
 const CreateStudentUtil = require('./utils/CreateStudentUtil');
