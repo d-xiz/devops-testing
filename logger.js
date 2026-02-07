6914e198bdbbf69c90bd1da4db4809e7b6a55c5d
d-xiz <danishmohamed2003@gmail.com>
2026-02-08 00:59:05 +0800

    Update logger.js

--- logger.js
+++ logger.js
@@ -5,12 +5,10 @@ const transports = [
 ];
 
 // Only write files when NOT testing
-if (process.env.NODE_ENV !== "test") {
   transports.push(
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" })
   );
-}
 
 const logger = winston.createLogger({
   level: "info",
